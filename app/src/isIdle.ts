b8681a4f0817f8b2d35f76568e895789c7366ab9
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2022-01-29 20:42:29 +0100

    code

--- app/src/isIdle.ts
+++ app/src/isIdle.ts
@@ -1,7 +1,14 @@
-dependencyImports
+import { Data } from 'josm';
 
-export function isIdle() {
-  
+export function isIdle(timeout: number) {
+  const isIdle = new Data(true) as any as Omit<Data<boolean>, "set"> & { stillActive: () => void }
+  let timoutID: NodeJS.Timeout
+  const timoutFunc = () => {(isIdle as any as Data<boolean>).set(true)}
+  isIdle.stillActive = () => {
+    (isIdle as any as Data<boolean>).set(false)
+    clearTimeout(timoutID)
+    timoutID = setTimeout(timoutFunc, timeout)
+  }
 }
 
 export default isIdle
